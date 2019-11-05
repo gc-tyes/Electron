481866d6fc1c6117f6f7ddc0c2867cd4706b335b
theterriblestidea <jessie.sui.js@gmail.com>
2019-11-05 17:15:26 -0500

    adding to realtime database

--- scripts/AuthorizeUsers.js
+++ scripts/AuthorizeUsers.js
@@ -9,17 +9,59 @@ var config = {
 
 // Initialize Firebase
 firebase.initializeApp(config);
-var db = firebase.database();
+var db = firebase.database();   // imports realtime database
 var auth = firebase.auth();
 
-// Form Handling
-function signUp() {
-    console.log("called");
-    var email = document.getElementById("email").value;
-    var psw = document.getElementById("psw").value;
-    auth.createUserWithEmailAndPassword(email, psw);
-    console.log("done");
+document.getElementById("signUpBtn").addEventListener("click", function () {
+    auth.onAuthStateChanged
+})
 
+// Form Handling
+async function signUp() {
+    console.log("called");
+    var email = document.getElementById("exampleInputEmail1").value;
+    var psw = document.getElementById("psw").value;
+    await auth.createUserWithEmailAndPassword(email, psw);
+
+    await auth.onAuthStateChanged(function (authUser) {
+        if (authUser) {
+            user(authUser.uid)
+                .once('value')
+                .then(snapshot => {
+                    const dbUser = snapshot.val();
+                    console.log(snapshot.val());
+                    console.log(authUser.uid);
+                    console.log(authUser.email);
+
+                    // merge auth and db user
+                    authUser = {
+                        uid: authUser.uid,
+                        email: authUser.email,
+                        ...dbUser,
+                    };
+                    user(authUser.uid).update({
+                        uid: authUser.uid,
+                        email: authUser.email,
+                        ...dbUser,
+                    })
+                }); 
+                console.log("done");
+        } else {
+            // No user is signed in.
+        }
+    });
+
+    //     await (authUser => {
+    //     console.log("reached");
+    //     // vanilla equiv of adding to db
+    //     return firebase
+    //         .user(authUser.user.uid)
+    //         .set({
+    //             username,
+    //             email,
+    //         });
+    // })
+    
     // todo: add to realtime database. turn this react code to vanilla js
 
     // this.props.firebase
@@ -46,20 +88,20 @@ function logIn() {
 // *** Auth API ***
 
 doCreateUserWithEmailAndPassword = (email, password) =>
-auth.createUserWithEmailAndPassword(email, password);
+    auth.createUserWithEmailAndPassword(email, password);
 
 doSignInWithEmailAndPassword = (email, password) =>
-auth.signInWithEmailAndPassword(email, password);
+    auth.signInWithEmailAndPassword(email, password);
 
 doSignOut = () => auth.signOut();
 
 doPasswordReset = email => auth.sendPasswordResetEmail(email);
 
 doPasswordUpdate = password =>
-auth.currentUser.updatePassword(password);
+    auth.currentUser.updatePassword(password);
 
 // *** User API ***
 
-User = uid => db.ref(`Users/${uid}`);
+user = uid => db.ref(`Users/${uid}`);
 
-Users = () => db.ref('Users');
+users = () => db.ref('Users');
