1298a1e10fca71acb3e111f486595581c56c7f9e
Mike Keohane <mkeohane@gatech.edu>
2020-01-14 19:43:31 -0500

    added basic framework for test software

---
+++ TestPages/TestMain.js
@@ -0,0 +1,9 @@
+// This method changes the screen to the survey results screen
+function toResults() {
+    document.location.href = "./TestResults.html"
+}
+
+// This method changes the screen to the test
+function toTest() {
+    document.location.href = "./Test.html"
+}
