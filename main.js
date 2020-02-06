6adfcbaae3deb567b748cf88c98b108e2d527898
lindsayashby <lashby6@gatech.edu>
2020-02-06 16:53:51 -0500

    all the stuff she worked on

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   // HERE IS WHERE YOU CAN CHANGE WHAT PAGE YOU START AT
-  win.loadFile('./TestPages/TestMain.html')
+  win.loadFile('./pages/TherapistDashboard1.0.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
